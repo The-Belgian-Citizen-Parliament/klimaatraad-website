e581ace6125c24c3f061d9e5d848c6351e88a0ef
Sels Vincent <vincent.sels@infrabel.be>
2020-09-08 18:29:49 +0200

    Database

---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongodb = require("mongodb");
+const ObjectID = mongodb.ObjectID;
+
+const MAILS_COLLECTION = "mails";
+
+const app = express();
+app.use(bodyParser.json());
+
+// Create a database variable outside of the database connection callback to reuse the connection pool in your app.
+var db;
+
+// Connect to the database before starting the application server.
+mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+  if (err) {
+    console.log(err);
+    process.exit(1);
+  }
+
+  // Save database object from the callback for reuse.
+  db = client.db();
+  console.log("Database connection ready");
+
+  // Initialize the app.
+  const server = app.listen(process.env.PORT || 8080, function () {
+    const port = server.address().port;
+    console.log("App now running on port", port);
+  });
+});
+
+// Generic error handler used by all endpoints.
+function handleError(res, reason, message, code) {
+  console.log("ERROR: " + reason);
+  res.status(code || 500).json({"error": message});
+}
+
+app.get("/api/mails/last", function(req, res) {
+  db.collection(MAILS_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get contacts.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+});
+
+app.post("/api/mails", function(req, res) {
+  const newMail = req.body;
+  newMail.createDate = new Date();
+
+  if (!req.body.body) {
+    handleError(res, "Invalid user input", "Must provide a body.", 400);
+  } else if (!req.body.subject) {
+    handleError(res, "Invalid user input", "Must provide a subject.", 400);
+  } else if (!req.body.to) {
+    handleError(res, "Invalid user input", "Must provide a 'to'.", 400);
+  } else {
+    db.collection(MAILS_COLLECTION).insertOne(newMail, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new contact.");
+      } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+});
