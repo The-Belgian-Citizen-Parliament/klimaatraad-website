963e2cf3f25c78a112edc19edde893508047ef2b
Vincent Sels <vincent_sels@hotmail.com>
2020-09-09 22:36:19 +0200

    Oops

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.post("/api/mails", function(req, res) {
         // Try to send mail
         if (process.env.MAILGUN_API_KEY) {
           try {
-            const mg = mailgun({ apiKey: process.env.MAILGUN_API_KE, domain: "klimaatraad.herokuapp.com" });
+            const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: "klimaatraad.herokuapp.com" });
             const data = {
               from: newMail.from,
               to: newMail.to,
