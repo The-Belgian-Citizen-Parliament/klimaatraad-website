b20d95a4250e310cbbfc36d74b8947348c3c2359
Vincent Sels <vincent_sels@hotmail.com>
2020-09-11 14:22:46 +0200

    Can't we just use a base url from the settings?

--- src/app/mail/mail.service.ts
+++ src/app/mail/mail.service.ts
@@ -2,24 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { Mail } from './mail';
-import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
 
 @Injectable()
 export class MailService {
-    private MailsUrl = '/api/mails';
+    private baseUrl = environment.baseUrl + '/api/mails';
 
-    constructor(private http: HttpClient, private router: Router) {}
+    constructor(private http: HttpClient) {}
 
     getLastMails(): Promise<void | Mail[]> {
       console.log('Retrieving latest emails... ');
-      return this.http.get<Mail[]>('http://localhost:4200' + this.MailsUrl + '/last')
+      return this.http.get<Mail[]>(this.baseUrl + '/last')
                  .toPromise()
                  .catch(this.handleError);
     }
 
     createMail(newMail: Mail): Promise<void | Mail> {
       console.log('Creating new email... ');
-      return this.http.post<Mail>('http://localhost:4200' + this.MailsUrl, newMail)
+      return this.http.post<Mail>(this.baseUrl, newMail)
                  .toPromise()
                  .catch(this.handleError);
     }
