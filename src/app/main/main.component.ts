5f45d0b06bde83fb571fd77f015ddfdfb67189c4
Vincent Sels <vincent_sels@hotmail.com>
2020-09-12 01:27:57 +0200

    Better front page

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { QuestionsService } from '../questions/questions.service';
+import { Question } from '../questions/questions';
 
 @Component({
   selector: 'app-main',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class MainComponent {
   iFrameLoaded = false;
+
+  questions: Question[] = [];
+
+  constructor(private questionsService: QuestionsService) {
+    this.questions = questionsService.getRandomQuestions(3);
+  }
+
+  moreQuestions = () => this.questions = this.questionsService.getRandomQuestions(3);
 }
