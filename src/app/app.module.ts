d5f3f71ce1a88b4cf7cf2875666d69b3d95b4d0c
Vincent Sels <vincent_sels@hotmail.com>
2020-09-15 16:47:46 +0200

    Add checkboxes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { TranslateModule } from '@ngx-translate/core';
@@ -37,6 +38,7 @@ import { QuestionComponent } from './questions/question/question.component';
     MatInputModule,
     MatSelectModule,
     MatButtonModule,
+    MatCheckboxModule,
     FlexLayoutModule,
     MatIconModule,
   ],
