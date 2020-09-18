d8bf5339662b87f8ead4bdae29c298cdfb795211
Vincent Sels <vincent_sels@hotmail.com>
2020-09-19 00:43:54 +0200

    Multi language routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,20 @@ import { MainComponent } from './main/main.component';
 import { MailComponent } from './mail/mail.component';
 import { FaqComponent } from './faq/faq.component';
 import { MemorandumComponent } from './memorandum/memorandum.component';
+import { environment } from 'src/environments/environment';
 
-const routes: Routes = [
+const routes: Routes = environment.language === 'nl' ? [
   { path: '', component: MainComponent },
   { path: 'memorandum', component: MemorandumComponent },
   { path: 'deelnemen', component: MailComponent },
   { path: 'vraag', component: FaqComponent },
+] : [
+  { path: '', component: MainComponent },
+  { path: 'memorandum', component: MemorandumComponent },
+  { path: 'participer', component: MailComponent },
+  { path: 'question', component: FaqComponent },
+  { path: 'deelnemen', redirectTo: 'participer' },
+  { path: 'vraag', redirectTo: 'question' },
 ];
 
 @NgModule({
