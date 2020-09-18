f9df886f08742007e742fecd0de96c41b070ec10
Vincent Sels <vincent_sels@hotmail.com>
2020-09-18 22:03:50 +0200

    HTML5 movie instead of YT; move to versio webspace for media

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://www.klimaatraad.be',
   language: 'nl',
-  cdnBaseUrl: 'https://belgiancitizenparliament.blob.core.windows.net/files',
+  cdnBaseUrl: 'http://vincentsels.be/ext/belgiancitizenparliament/img',
 };
