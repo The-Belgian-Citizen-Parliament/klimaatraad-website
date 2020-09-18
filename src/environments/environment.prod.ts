19461eb287c7c9e5e0c1a2221113929a46495b8d
Vincent Sels <vincent_sels@hotmail.com>
2020-09-18 17:19:35 +0200

    Must update in all environment files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,5 @@ export const environment = {
   production: true,
   baseUrl: 'https://www.klimaatraad.be',
   language: 'nl',
+  cdnBaseUrl: 'https://belgiancitizenparliament.blob.core.windows.net/files',
 };
