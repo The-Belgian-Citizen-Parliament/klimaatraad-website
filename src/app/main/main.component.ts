ebb14e0378ea442de2800cc9878e7629e38c51ad
Vincent Sels <vincent_sels@hotmail.com>
2020-09-21 18:06:57 +0200

    Started on video carrousel

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,8 +12,19 @@ export class MainComponent {
 
   questions: Question[] = [];
 
+  videos = [
+    { nr: 1, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/maaike.mp4', poster: '/assets/vidposters/maaike.jpg' },
+    { nr: 2, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn1.mp4', poster: '/assets/vidposters/nn1.jpg' },
+    { nr: 3, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn2.mp4', poster: '/assets/vidposters/nn2.jpg' },
+    { nr: 4, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn3.mp4', poster: '/assets/vidposters/nn3.jpg' },
+    { nr: 5, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn4.mp4', poster: '/assets/vidposters/nn4.jpg' },
+  ];
+
+  currentVideo;
+
   constructor(private questionsService: QuestionsService) {
     this.questions = questionsService.getRandomQuestions(3);
+    this.currentVideo = this.videos[0];
   }
 
   moreQuestions = () => this.questions = this.questionsService.getRandomQuestions(3);
