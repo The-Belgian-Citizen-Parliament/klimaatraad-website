c30848b19be42e880d97dcce08747f138a32378b
Vincent Sels <vincent_sels@hotmail.com>
2020-09-21 19:18:39 +0200

    Auto play on select works

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { QuestionsService } from '../questions/questions.service';
 import { Question } from '../questions/questions';
 
@@ -8,24 +8,31 @@ import { Question } from '../questions/questions';
   styleUrls: ['./main.component.scss']
 })
 export class MainComponent {
+  @ViewChild('videoPlayer') videoplayer: ElementRef;
+
   iFrameLoaded = false;
 
   questions: Question[] = [];
 
   videos = [
-    { nr: 1, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/maaike.mp4', poster: '/assets/vidposters/maaike.jpg' },
-    { nr: 2, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn1.mp4', poster: '/assets/vidposters/nn1.jpg' },
-    { nr: 3, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn2.mp4', poster: '/assets/vidposters/nn2.jpg' },
-    { nr: 4, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn3.mp4', poster: '/assets/vidposters/nn3.jpg' },
-    { nr: 5, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn4.mp4', poster: '/assets/vidposters/nn4.jpg' },
+    { nr: 0, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn1.mp4', poster: '/assets/vidposters/nn1.jpg' },
+    { nr: 1, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn2.mp4', poster: '/assets/vidposters/nn2.jpg' },
+    { nr: 2, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn3.mp4', poster: '/assets/vidposters/nn3.jpg' },
+    { nr: 3, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/nn4.mp4', poster: '/assets/vidposters/nn4.jpg' },
+    { nr: 4, src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/maaike.mp4', poster: '/assets/vidposters/maaike.jpg' },
   ];
 
   currentVideo;
 
   constructor(private questionsService: QuestionsService) {
     this.questions = questionsService.getRandomQuestions(3);
-    this.currentVideo = this.videos[0];
+    this.currentVideo = this.videos[4];
   }
 
   moreQuestions = () => this.questions = this.questionsService.getRandomQuestions(3);
+
+  playVid(vid) {
+    this.currentVideo = vid;
+    setTimeout(() => this.videoplayer.nativeElement.play());
+  }
 }
