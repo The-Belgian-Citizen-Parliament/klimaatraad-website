7e5bd163902beda8a65b3630953858baf69f056d
Vincent Sels <vincent_sels@hotmail.com>
2020-10-04 23:37:53 +0200

    Fix mail warning

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -60,7 +60,8 @@ export function bootstrap(app: express.Express) {
             console.log('Sending mail...');
             const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain: process.env.MAILGUN_DOMAIN, host: 'api.eu.mailgun.net' });
             const data = {
-              from: newMail.firstName + ' ' + newMail.lastName + '<' + newMail.email + '>',
+              from: newMail.firstName + ' ' + newMail.lastName + ' <info@thecitizensparliament.be>', //  '<' + newMail.email + '>',
+              'h:Reply-To': newMail.email,
               to: newMail.to,
               subject: newMail.subject,
               text: newMail.body,
