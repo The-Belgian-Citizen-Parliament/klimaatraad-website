32616a31eb482e0087a990aff7156b9e4e5b462a
Vincent Sels <vincent_sels@hotmail.com>
2020-10-17 12:31:30 +0200

    Add titles to videos

--- src/app/main/videos.ts
+++ src/app/main/videos.ts
@@ -4,103 +4,137 @@ export const videos = {
       nr: 0,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/youna_nl.mp4',
       poster: '/assets/vidposters/youna.jpg',
-      preview: '/assets/vidposters/youna_square.jpg'
+      preview: '/assets/vidposters/youna_square.jpg',
+      name: 'Youna Marette',
+      title: 'Génération Climat, woordvoerster Het Burgerparlement'
     },
     {
       nr: 1,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/sebastien_nl.mp4',
       poster: '/assets/vidposters/sebastien.jpg',
-      preview: '/assets/vidposters/sebastien_square.jpg'
+      preview: '/assets/vidposters/sebastien_square.jpg',
+      name: 'Sébastien Hendrickx',
+      title: 'docent, woordvoerder Het Burgerparlement'
     },
     {
       nr: 2,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/esmeralda_nl.mp4',
       poster: '/assets/vidposters/esmeralda.jpg',
-      preview: '/assets/vidposters/esmeralda_square.jpg'
+      preview: '/assets/vidposters/esmeralda_square.jpg',
+      name: 'Prinses Marie Esmeralda van België',
+      title: 'dochter van koning Leopold III'
     },
     {
       nr: 3,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Anuna_Quote.mp4',
       poster: '/assets/vidposters/anuna.jpg',
-      preview: '/assets/vidposters/anuna_square.jpg'
+      preview: '/assets/vidposters/anuna_square.jpg',
+      name: 'Anuna De Wever',
+      title: 'Youth For Climate'
     },
     {
       nr: 4,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Dirk_Quote.mp4',
       poster: '/assets/vidposters/dirk.jpg',
-      preview: '/assets/vidposters/dirk_square.jpg'
+      preview: '/assets/vidposters/dirk_square.jpg',
+      name: 'Dirk Draulans',
+      title: 'bioloog'
     },
     {
       nr: 5,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Khalid_Quote2.mp4',
       poster: '/assets/vidposters/khalid.jpg',
-      preview: '/assets/vidposters/khalid_square.jpg'
+      preview: '/assets/vidposters/khalid_square.jpg',
+      name: 'Khalid Benhaddou',
+      title: 'imam'
     },
     {
       nr: 6,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Maaike_Quote.mp4',
       poster: '/assets/vidposters/maaike.jpg',
-      preview: '/assets/vidposters/maaike_square.jpg'
+      preview: '/assets/vidposters/maaike_square.jpg',
+      name: 'Maaike Neuville',
+      title: 'actrice'
     },
     {
       nr: 7,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Marleen_Quote.mp4',
       poster: '/assets/vidposters/marleen.jpg',
-      preview: '/assets/vidposters/marleen_square.jpg'
+      preview: '/assets/vidposters/marleen_square.jpg',
+      name: 'Marleen Merckx',
+      title: 'actrice'
     },
     {
       nr: 8,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Veerle_Quote.mp4',
       poster: '/assets/vidposters/veerle.jpg',
-      preview: '/assets/vidposters/veerle_square.jpg'
+      preview: '/assets/vidposters/veerle_square.jpg',
+      name: 'Veerle Baetens',
+      title: 'actrice'
     },
     {
       nr: 9,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/david_nl.mp4',
       poster: '/assets/vidposters/david.jpg',
-      preview: '/assets/vidposters/david_square.jpg'
+      preview: '/assets/vidposters/david_square.jpg',
+      name: 'David Van Reybrouck',
+      title: 'cultuurhistoricus, auteur \'Tegen Verkiezingen\''
     }],
   fr: [
     {
       nr: 0,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/youna_fr.mp4',
       poster: '/assets/vidposters/youna.jpg',
-      preview: '/assets/vidposters/youna_square.jpg'
+      preview: '/assets/vidposters/youna_square.jpg',
+      name: 'Youna Marette',
+      title: 'Génération Climat, porte-parole Le Parlement Citoyen'
     },
     {
       nr: 1,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/sebastien_fr.mp4',
       poster: '/assets/vidposters/sebastien.jpg',
-      preview: '/assets/vidposters/sebastien_square.jpg'
+      preview: '/assets/vidposters/sebastien_square.jpg',
+      name: 'Sébastien Hendrickx',
+      title: 'docent, porte-parole Le Parlement Citoyen'
     },
     {
       nr: 2,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/esmeralda_fr.mp4',
       poster: '/assets/vidposters/esmeralda.jpg',
-      preview: '/assets/vidposters/esmeralda_square.jpg'
+      preview: '/assets/vidposters/esmeralda_square.jpg',
+      name: 'Princesse Marie-Esméralda de Belgique',
+      title: 'fille du roi Leopold III'
     },
     {
       nr: 3,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Benjamin_Quote.mp4',
       poster: '/assets/vidposters/benjamin.jpg',
-      preview: '/assets/vidposters/benjamin_square.jpg'
+      preview: '/assets/vidposters/benjamin_square.jpg',
+      name: 'Benjamin Ramon',
+      title: 'acteur'
     },
     {
       nr: 4,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Anne_Quote.mp4',
       poster: '/assets/vidposters/anne.jpg',
-      preview: '/assets/vidposters/anne_square.jpg'
+      preview: '/assets/vidposters/anne_square.jpg',
+      name: 'Anne Coesens',
+      title: 'actrice'
     },
     {
       nr: 5,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/Veerle_QuoteSUB.mp4',
       poster: '/assets/vidposters/veerle.jpg',
-      preview: '/assets/vidposters/veerle_square.jpg'
+      preview: '/assets/vidposters/veerle_square.jpg',
+      name: 'Veerle Baetens',
+      title: 'actrice'
     },
     {
       nr: 6,
       src: 'https://vincentsels.be/ext/belgiancitizenparliament/video/david_fr.mp4',
       poster: '/assets/vidposters/david.jpg',
-      preview: '/assets/vidposters/david_square.jpg'
+      preview: '/assets/vidposters/david_square.jpg',
+      name: 'David Van Reybrouck',
+      title: 'historien de la culture, auteur \'Contre les Elections\''
     }],
 };
